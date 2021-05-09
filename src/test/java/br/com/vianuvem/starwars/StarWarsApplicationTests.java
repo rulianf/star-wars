6acd16864b94b6e11445e37f03570cc6e5fa394c
Rulian Felipp <rulianf@gmail.com>
2021-05-09 17:47:04 -0300

    Inicio do projeto

---
+++ src/test/java/br/com/vianuvem/starwars/StarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vianuvem.starwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
